7c974f1a465ebe03820f8c84870869c7e14024e7
nfujita0707 <nfujita0714@gmail.com>
2021-08-17 06:30:11 +0000

    共通化実施

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,5 @@
 class PostsController < ApplicationController
   def index
-    @posts = [
-      "今日はRailsの学習です。"
-      "プログラミング意外と簡単。"
-      "この調子で続けていこう。"
-    ]
+    @posts = Post.all
   end
 end
