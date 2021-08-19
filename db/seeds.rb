ed7899b9a2ae7119c25a0da5ca223628443c2b67
nfujita0707 <nfujita0714@gmail.com>
2021-08-19 05:27:02 +0000

    ユーザー一覧インポート

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+posts = [
+  '今日からRailsの学習を始めます。',
+  '毎日Rails学習中！プログラミング楽しい！',
+  'だんだん内容が難しくなってきたぞ。',
+  'レッスンで作っているアプリが形になってきたな～、この調子だ！',
+  'どこでもプログラミングできるように、ノートパソコン買っちゃおうかな。',
+]
+
+posts.each { |post| Post.create!(content: post) }
+
+User.create!(name: 'フリーエンジニア君', email: 'engineer2@email.com')
+User.create!(name: 'フリーデザイナー君', email: 'designer@email.com')
+User.create!(name: 'アルパカさん', email: 'alpaca@email.com')
+User.create!(name: 'ゴリラくん', email: 'golilla@email.com')
+User.create!(name: 'セレブエンジニアさん', email: 'celeb@email.com')
