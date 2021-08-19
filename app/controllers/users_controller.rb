1a24e3e2e8fabb77fe4430a91e90ec1db88e3b9e
nfujita0707 <nfujita0714@gmail.com>
2021-08-19 11:15:14 +0000

    ログイン画面編集機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
   end
 
   def create
@@ -19,5 +19,14 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def edit
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
   end
 end
