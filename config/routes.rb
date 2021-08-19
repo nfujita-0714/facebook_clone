bbbfae4e3c8e7153b4c02e59021eb28bd2d95db7
nfujita0707 <nfujita0714@gmail.com>
2021-08-19 03:16:02 +0000

    routes.rbにresources :usersを追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
   get  'posts/:id/edit', to: 'posts#edit', as: :edit_post
   post  'posts/:id/update', to: 'posts#update', as: :update_post
   post  'posts/:id/destroy', to: 'posts#destroy', as: :destroy_post
+
+  resources :users
 end
