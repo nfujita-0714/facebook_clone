62c7c51053d8784ee59beb9ba4062d79cb7b9004
nfujita0707 <nfujita0714@gmail.com>
2021-08-20 07:50:29 +0000

    ユーザーと写真の関連付け・404、500エラー実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
+  before_action :limitation_correct_user, only: [:edit, :update, :destroy]
+
   def new
     unless @current_user
       flash[:notice] = "ログインしてください。"
@@ -27,15 +29,13 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-    @user = User.find(@post.user_id)
+    @user = @post.user
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     @post.content =  params[:content]
     if @post.save
       redirect_to posts_index_url
@@ -46,9 +46,16 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     flash[:notice] = "投稿を削除しました！"
     redirect_to posts_index_url
+  end
+
+  def limitation_correct_user
+    @post = Post.find(params[:id])
+    unless @post.user_id == @current_user.id
+      flash[:notice]= "自分意外のユーザーの投稿は編集できません。"
+      redirect_to posts_index_url
+    end
   end
 end
